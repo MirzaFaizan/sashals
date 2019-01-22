eac5a00b7c7a1eb8573f950ceb0daa1e503fbcd2
Faizan <mirzafaizanejaz@gmail.com>
2019-01-22 16:32:52 +0500

    added Atlas DB

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 const config = {};
 
-config.mongodbHost = 'mongodb://localhost:27017/';
-config.mongodbName = 'b2b';
+config.mongodbHost = 'mongodb://faizi:faizi@cluster0-shard-00-00-aquse.mongodb.net:27017,cluster0-shard-00-01-aquse.mongodb.net:27017,cluster0-shard-00-02-aquse.mongodb.net:27017/sashals?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
+config.mongodbHostLocal = 'mongodb://localhost:27017/';
+config.mongodbName = 'sashals';
 
 module.exports = config;
